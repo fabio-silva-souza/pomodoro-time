e89e73d17a2317275c63e9630861b80db43a727e
fabio-silva-souza <silvafabiosouza@hotmail.com>
2022-09-01 22:02:44 -0300

    c..

---
+++ cron.js
@@ -0,0 +1,44 @@
+let initial;
+
+let min = 0;
+let sec = 0;
+
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+
+document.getElementById('start').onclick = () => start();
+document.getElementById('pause').onclick = () => pause();
+document.getElementById('reset').onclick = () => reset();
+
+function start()
+{ 
+    pause();
+    initial = setInterval(timer, 10);
+}
+function pause()
+{
+    clearInterval(initial);
+}
+function reset()
+{
+    min = 0;
+    sec = 0;
+    minute.innerText = '00';
+    second.innerText = '00';
+    clearInterval(initial);
+}
+function timer()
+{
+    sec++;
+    if(sec == 60)
+    {
+        sec = 00;
+        min++;
+    }
+    minute.innerText = returnZero(min);
+    second.innerText = returnZero(sec);
+}
+function returnZero(value) 
+{
+return value > 9 ? value : `0${value}`
+}
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let initial;
 let min = 24;
 let sec = 60;
 
-let minEx = [4 , 4 , 15 , 30]
+
 let minuteExtra = document.getElementById("minute-2")
 
 let contains = document.getElementById("contains")
@@ -18,32 +18,27 @@ document.getElementById('pause').onclick = () => pause();
 function start()
 { 
     pause();
-    initial = setInterval(timer, 10);
+    initial = setInterval(timer, 1000);
 }
 function pause()
 {
     clearInterval(initial);
 }
-function reset()
-{
-    min = 24;
-    sec = 60;
-    minute.innerText = '25';
-    second.innerText = '00';
-}
+// function reset()
+// {
+//     min = ;
+//     sec = ;
+//     minute.innerText = '25';
+//     second.innerText = '00';
+// }
 function timer()
 {
-    sec--;
-    if(sec == 0)
+    sec++;
+    if(sec == 60)
     {
-        sec = 60;
-        min--;
+        sec = 00;
+        min++;
     }
-    else if(min == 0)
-    {
-        winTimer();
-    }
-    
     
     minute.innerText = returnZero(min);
     second.innerText = returnZero(sec);
